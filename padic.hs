594d1ef96df121e6d6484d1533218b509055a95b
M42 <mromang08@gmail.com>
2016-08-29 18:13:59 +0200

    Instancias para números p-ádicos

--- padic.hs
+++ padic.hs
@@ -1,29 +1,49 @@
-type Padic = [Int]
+{-# LANGUAGE TypeSynonymInstances #-}
 
+data Padic = Padic [Integer]
+
+p :: (Num a) => a
 p = 3
 
-normalize :: [Int] -> Padic
+normalize :: (Integral a) => [a] -> [a]
 normalize = normalize' 0
   where
     normalize' 0     []     = []
     normalize' carry []     = normalize' 0 [carry]
-    normalize' carry (x:xs) = (mod (x+carry) p) : (normalize' (quot (x+carry) p) xs)
+    normalize' carry (x:xs) = (mod (x+carry) p) : (normalize' (div (x+carry) p) xs)
 
-toPadic :: Int -> Padic
+toPadic :: (Integral a) => a -> [a]
 toPadic n = normalize [n]
 
-toInt :: Padic -> Int
-toInt []     = 0
-toInt (x:xs) = x + p * (toInt xs)
+opposite :: (Integral a) => [a] -> [a]
+opposite xs = normalize $ map (negate) xs 
 
-plus :: Padic -> Padic -> Padic
+ptoInt :: (Num a) => [a] -> a
+ptoInt []     = 0
+ptoInt (x:xs) = x + p * (ptoInt xs)
+
+plus :: (Integral a) => [a] -> [a] -> [a]
 plus xs     []     = normalize $ xs
 plus []     ys     = normalize $ ys
 plus (x:xs) (y:ys) = normalize $ (x+y) : (plus xs ys)
 
-sum :: [Padic] -> Padic
+sum :: (Integral a) => [[a]] -> [a]
 sum = foldr plus [0]
 
-prod :: Padic -> Padic -> Padic
+prod :: (Integral a) => [a] -> [a] -> [a] 
 prod []     ys = [0]
 prod (x:xs) ys = plus (map (*x) ys) (prod xs (0:ys))
+
+
+instance Num Padic where
+  (Padic a) + (Padic b) = Padic (plus a b)
+  negate (Padic a) = Padic (opposite a)
+  (-) a b = (+) a (negate b)
+  (Padic a) * (Padic b) = Padic (prod a b)
+  abs = id
+  signum = (\_ -> 1)
+  fromInteger x = Padic (toPadic x)
+
+instance Enum Padic where
+  toEnum n = Padic (toPadic (toInteger n))
+  fromEnum (Padic xs) = ptoInt (map fromInteger xs) 
